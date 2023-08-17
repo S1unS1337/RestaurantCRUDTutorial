6c27fe095130944bc6c1a3bf9115a7b0cb4f0967
Illia Teliuk <illiateliuk@gmail.com>
2023-08-17 22:43:52 +0300

    moved some functions to operate with OrderList and popup window

--- restaurant-app/src/components/Order/OrderList.js
+++ restaurant-app/src/components/Order/OrderList.js
@@ -4,7 +4,9 @@ import Table from '../layouts/Table'
 import { Icon, TableBody, TableCell, TableHead, TableRow } from '@mui/material'
 import DeleteOutlineTwoToneIcon from '@mui/icons-material/DeleteOutlineTwoTone';
 
-export default function OrderList() {
+export default function OrderList(props) {
+
+    const {setOrderId, setOrderListVisibility} = props
 
     const [orderList, setOrderList] = useState([])
 
@@ -15,6 +17,11 @@ export default function OrderList() {
         })
         .catch(err => console.log(err))
     }, [])
+
+    const showForUpdate = id => {
+        setOrderId(id)
+        setOrderListVisibility(false)
+    }
 
   return (
     <Table>
@@ -31,16 +38,25 @@ export default function OrderList() {
             {
                 orderList.map(item => (
                     <TableRow key = {item.orderMasterId}>
-                        <TableCell>
+                        <TableCell
+                            onClick={e => showForUpdate(item.orderMasterId)}
+                        >
                             #{item.orderNumber}
                         </TableCell>
-                        <TableCell>
+                        <TableCell
+                            onClick={e => showForUpdate(item.orderMasterId)}
+
+                        >
                             {item.customer.customerName}
                         </TableCell>
-                        <TableCell>
+                        <TableCell
+                            onClick={e => showForUpdate(item.orderMasterId)}            
+                        >
                             {item.pMethod}
                         </TableCell>
-                        <TableCell>
+                        <TableCell
+                            onClick={e => showForUpdate(item.orderMasterId)}
+                        >
                             {item.gTotal} $
                         </TableCell>
                         <TableCell>
